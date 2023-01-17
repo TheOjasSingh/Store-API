727b4d942ec4c7dd97eb8acdd85bb7d5083b5693
Ojas Singh <ojas856@gmail.com>
2023-01-17 22:50:54 +0530

    Update Product.js
    Created Sort function

--- controllers/products.js
+++ controllers/products.js
@@ -6,7 +6,7 @@ const getALLProductsStatic = async (req, res)=>{
 }
 
 const getAllProducts = async (req, res)=> {
-    const {featured, company, name} = req.query
+    const {featured, company, name, sort} = req.query
     const queryObject = {}
     if(featured){
         queryObject.featured = featured === 'true' ? true : false
@@ -17,7 +17,15 @@ const getAllProducts = async (req, res)=> {
     if (name) {
         queryObject.name = { $regex: name, $options: 'i'}
     }
-    const products = await Product.find(queryObject)
+    let result = Product.find(queryObject)
+    if (sort) {
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    }
+    else{
+        result = result.sort('createAt')
+    }
+    const products = await result 
     res.status(200).json({products, nbHints : products.length})
 }
 

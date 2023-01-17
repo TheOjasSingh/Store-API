3f9c33dfb06d0e367361beed4dc3a4fd14134299
Ojas Singh <ojas856@gmail.com>
2023-01-17 21:30:45 +0530

    Forth Commit
    Search feature added

--- controllers/products.js
+++ controllers/products.js
@@ -2,11 +2,23 @@ const Product = require('../models/product')
 
 const getALLProductsStatic = async (req, res)=>{
     const products = await Product.find({featured:true})
-    res.status(200).json({products})
+    res.status(200).json({products, nbHints : products.length})
 }
 
 const getAllProducts = async (req, res)=> {
-    res.status(200).json({msg:'products route'})
+    const {featured, company, name} = req.query
+    const queryObject = {}
+    if(featured){
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    if (company) {
+        queryObject.company = company
+    }
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i'}
+    }
+    const products = await Product.find(queryObject)
+    res.status(200).json({products, nbHints : products.length})
 }
 
 module.exports = {

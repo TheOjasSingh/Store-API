a63ffb15a87007e82634d4812e305d6c69c95d6c
Ojas Singh <ojas856@gmail.com>
2023-01-18 21:00:40 +0530

    Update product.js
    Added sort and NumericFilters

--- controllers/products.js
+++ controllers/products.js
@@ -6,8 +6,9 @@ const getALLProductsStatic = async (req, res)=>{
 }
 
 const getAllProducts = async (req, res)=> {
-    const {featured, company, name, sort} = req.query
+    const {featured, company, name, sort, fields, numericFilters} = req.query
     const queryObject = {}
+    //Search
     if(featured){
         queryObject.featured = featured === 'true' ? true : false
     }
@@ -17,7 +18,29 @@ const getAllProducts = async (req, res)=> {
     if (name) {
         queryObject.name = { $regex: name, $options: 'i'}
     }
+    //Numeric_Filters
+    if (numericFilters) {
+        const operatorMap = {
+            '>':'$gt',
+            '>=':'$gte',
+            '=':'$e',
+            '<':'$lt',
+            '<=':'$lte',
+
+        }
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g
+        let filters = numericFilters.replace(regEx, (match)=>`-${operatorMap[match]}-`)
+        const options = ['price','rating']
+        filters = filters.split(',').forEach((item)=>{
+            const [field,operator,value] = item.split('-')
+            if (options.includes(field)) {
+                queryObject[field] = {[operator]:Number(value)}                
+            }
+        })
+    }
+    console.log(queryObject)
     let result = Product.find(queryObject)
+    //sort
     if (sort) {
         const sortList = sort.split(',').join(' ')
         result = result.sort(sortList)
@@ -25,6 +48,17 @@ const getAllProducts = async (req, res)=> {
     else{
         result = result.sort('createAt')
     }
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)
+    }
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+
+    result = result.skip(skip).limit(limit)
+
+
     const products = await result 
     res.status(200).json({products, nbHints : products.length})
 }
